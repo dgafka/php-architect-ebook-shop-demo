402bbb1c6c0d11cd9e5b4ccfd1ffee7daea19e09
Dariusz Gafka <dariuszgafka@gmail.com>
2022-02-20 12:07:24 +0100

    View refactor

--- 1-lesson/src/PaymentGateway.php
+++ 1-lesson/src/PaymentGateway.php
@@ -6,6 +6,6 @@ class PaymentGateway
 {
     public function performPayment(array $creditCard, float $amount): void
     {
-        echo sprintf("Payment was performed for %d using credit card number: %s, valid till: %s/%s and cvc: %s", $amount, $creditCard["number"], $creditCard["validTillMonth"], $creditCard["validTillYear"], $creditCard["cvc"]);
+        echo sprintf("Payment was performed for %d using credit card number: %s, valid till: %s/%s and cvc: %s\n", $amount, $creditCard["number"], $creditCard["validTillMonth"], $creditCard["validTillYear"], $creditCard["cvc"]);
     }
 }
